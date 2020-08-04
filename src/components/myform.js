6c8b1a59b48f35f464188e7fb85218edadd1adf4
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-08-04 15:42:56 +0200

    contact api fixed

--- src/components/myform.js
+++ src/components/myform.js
@@ -11,50 +11,50 @@ export default class MyForm extends React.Component {
 
 	render() {
 		return (
-            <div className= "contact-form">
-			  <form
-				id="fs-frm"
-				name="simple-contact-form"
-				acceptCharset="utf-8"
-				action="https://formspree.io/YOUR_EMAIL_HERE"
-				method="post"
-			  >
-				<fieldset id="fs-frm-inputs">
-					<label htmlFor="full-name">Full Name</label>
-					<input
-						type="text"
-						name="name"
-						id="full-name"
-						placeholder="First and Last"
-						required
-					/>
-					<label htmlFor="email-address">Email Address</label>
-					<input
-						type="email"
-						name="_replyto"
-						id="email-address"
-						placeholder="email@name.com..."
-						required
-					/>
-					<label htmlFor="message">Message</label>
-					<textarea
-						rows={5}
-						name="message"
-						id="message"
-						placeholder="Write your message here..."
-						required
-						defaultValue={""}
-					/>
-					<input
-						type="hidden"
-						name="_subject"
-						id="email-subject"
-						defaultValue="Contact Form Submission"
-					/>
-				</fieldset>
-				<input type="submit" defaultValue="Submit" />
-		    	</form>
-            </div>
+			<div className="contact-form">
+				<form
+					id="fs-frm"
+					name="simple-contact-form"
+					acceptCharset="utf-8"
+					action="https://formspree.io/xvowbjaj"
+					method="post"
+				>
+					<fieldset id="fs-frm-inputs">
+						<label htmlFor="full-name">Full Name</label>
+						<input
+							type="text"
+							name="name"
+							id="full-name"
+							placeholder="First and Last"
+							required
+						/>
+						<label htmlFor="email-address">Email Address</label>
+						<input
+							type="email"
+							name="_replyto"
+							id="email-address"
+							placeholder="email@name.com..."
+							required
+						/>
+						<label htmlFor="message">Message</label>
+						<textarea
+							rows={5}
+							name="message"
+							id="message"
+							placeholder="Write your message here..."
+							required
+							defaultValue={""}
+						/>
+						<input
+							type="hidden"
+							name="_subject"
+							id="email-subject"
+							defaultValue="Contact Form Submission"
+						/>
+					</fieldset>
+					<input type="submit" defaultValue="Submit" />
+				</form>
+			</div>
 		);
 	}
 
